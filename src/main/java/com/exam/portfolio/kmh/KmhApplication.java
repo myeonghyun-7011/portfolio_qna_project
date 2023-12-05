4b85f46d90f68e595b0d523ec8b3f13683f65811
myeonghyun-7011 <dhvlftmd7013@naver.com>
2023-12-05 10:03:05 +0900

    세팅

---
+++ src/main/java/com/exam/portfolio/kmh/KmhApplication.java
@@ -0,0 +1,14 @@
+package com.exam.portfolio.kmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmhApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
